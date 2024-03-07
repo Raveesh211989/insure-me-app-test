7cc7b69ba9dcd2668291317b18ecb807a0e6e3a3
Raveesh211989 <158907853+Raveesh211989@users.noreply.github.com>
2024-03-07 11:17:45 -0500

    Update App.java

--- src/main/java/com/capstone/test/App.java
+++ src/main/java/com/capstone/test/App.java
@@ -18,7 +18,7 @@ public class App
         chromeOptions.addArguments("--headless");
         System.out.println("Test Case is strting");
         WebDriver driver = new ChromeDriver(chromeOptions);
-	System.out.println(WebDriver driver = new ChromeDriver(chromeOptions);
+	System.out.println("Chromedriver newchro");
         driver.get("http://54.226.78.205:8085/");
 	System.out.println("URL Open");
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
